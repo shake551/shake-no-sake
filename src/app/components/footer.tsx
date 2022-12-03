2b65db2659a3ef9fdab5902b463c29520b979e54
shake <hiroki.yamako@ezweb.ne.jp>
2022-12-03 17:36:47 +0900

    add shop footer

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -1,5 +1,5 @@
 import {memo} from 'react';
-import {Home, List, PlusCircle} from 'react-feather';
+import {Home, List, PlusCircle, ShoppingCart} from 'react-feather';
 
 import styles from './footer.module.css';
 
@@ -12,3 +12,10 @@ export const CocktailFooter = memo(() => (
         <List size={80} color={accentColor} />
     </footer>
 ));
+
+export const ShopFooter = memo(() => (
+    <footer className={styles.footer}>
+        <Home size={80} color={accentColor} />
+        <ShoppingCart size={80} color={accentColor} />
+    </footer>
+))
