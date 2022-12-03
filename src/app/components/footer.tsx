a4d81d2b58bc308125635439fc8455ff820b0759
shake <hiroki.yamako@ezweb.ne.jp>
2022-12-03 17:25:59 +0900

    update footer name

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -5,7 +5,7 @@ import styles from './footer.module.css';
 
 const accentColor = 'silver';
 
-export const Footer = memo(() => (
+export const CocktailFooter = memo(() => (
     <footer className={styles.footer}>
         <Home size={80} color={accentColor} />
         <PlusCircle size={80} color={accentColor} />
