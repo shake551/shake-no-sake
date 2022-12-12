43787e98e96f911944c126dd03cc04a7126697a3
shake551 <73379887+shake551@users.noreply.github.com>
2022-12-12 20:55:05 +0900

    add link to detail page

--- src/app/cocktails/page.tsx
+++ src/app/cocktails/page.tsx
@@ -2,6 +2,7 @@ import RootLayout from "../layout/cocktail";
 import Image from "next/image";
 
 import styles from './cocktails.module.css';
+import Link from "next/link";
 
 async function getCocktailsList() {
     const res = await fetch('http://host.docker.internal/cocktails', {method: 'GET'});
@@ -15,16 +16,18 @@ export default async function Page() {
             <ul className={styles.cocktailsCardList}>
                 {cocktailsList.map(({id, name}: { id: number, name: string }) => (
                     <li className={styles.cocktailsCardWrapper} key={id}>
-                        <div className={styles.cocktailsCard}>
-                            <Image
-                                src="/cocktail_sample.jpg"
-                                alt="cocktail sample image"
-                                width={200}
-                                height={200}
-                                className={styles.cocktailsCardImage}
-                            />
-                            <h1 className={styles.cocktailsCardName}>{name}</h1>
-                        </div>
+                        <Link href={`/cocktails/${id}`}>
+                            <button className={styles.cocktailsCard}>
+                                <Image
+                                    src="/cocktail_sample.jpg"
+                                    alt="cocktail sample image"
+                                    width={200}
+                                    height={200}
+                                    className={styles.cocktailsCardImage}
+                                />
+                                <h1 className={styles.cocktailsCardName}>{name}</h1>
+                            </button>
+                        </Link>
                     </li>
                 ))}
             </ul>
