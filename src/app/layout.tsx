edddbd5877d2a6aa317ea11bea920f5dd38ceddd
shake551 <73379887+shake551@users.noreply.github.com>
2022-12-14 13:45:57 +0900

    update layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,16 @@
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
-  return (
-    <html>
-      <head></head>
-      <body>{children}</body>
-    </html>
-  )
+import './globals.css'
+import {CocktailFooter} from "./components/cocktail_footer";
+
+export default function RootLayout({ children, page }: { children: React.ReactNode, page: {name: string, id: number | null} }) {
+    return (
+        <html lang="ja">
+        <head>
+            <title>shake no sake</title>
+            <meta name="description" content="shake no sake"/>
+            <link rel="icon" href="/favicon.ico"/>
+        </head>
+        <body>{children}</body>
+        { page.name == 'cocktail' ? <CocktailFooter /> : <></>}
+        </html>
+    )
 }
