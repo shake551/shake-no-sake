712a64562cbcd79f25d4b0316981b66ea799bef9
shake <hiroki.yamako@ezweb.ne.jp>
2022-12-31 18:08:30 +0900

    add to localStorage

--- src/app/components/shop/add_cart_button.tsx
+++ src/app/components/shop/add_cart_button.tsx
@@ -1,9 +1,25 @@
+'use client';
 import styles from './add_cart_button.module.css';
+import Link from "next/link";
 
-export default function AddCartButton() {
+export default function AddCartButton(params: { shopId: string, cocktailId: string }) {
+    const onClick = () => {
+        let exist = localStorage.getItem("cart");
+        let newArray;
+        if (exist) {
+            let array = JSON.parse(exist);
+            newArray = array.concat({"id": params.cocktailId, "count": 1});
+        } else {
+            newArray = [params.cocktailId];
+        }
+        localStorage.setItem("cart", JSON.stringify(newArray));
+    }
+    
     return (
         <div className={styles.cartButtonWrapper}>
-            <button className={styles.addCartButton}>カートに追加</button>
+            <Link href={`/shop/${params.shopId}/cocktail`}>
+                <button className={styles.addCartButton} onClick={onClick}>カートに追加</button>
+            </Link>
         </div>
     )
 }
