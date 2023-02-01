354b263def799d3775fb8e1472737594fc7223cf
shake551 <73379887+shake551@users.noreply.github.com>
2023-02-01 23:46:36 +0900

    add order button

--- src/app/shop/[shopId]/table/[tableId]/cart/page.tsx
+++ src/app/shop/[shopId]/table/[tableId]/cart/page.tsx
@@ -1,9 +1,12 @@
 'use client';
-import RootLayout from "../../../layout";
+import RootLayout from "../../../../../layout";
 import {useEffect, useState} from "react";
-import CocktailCartList from "../../../components/cocktail/cocktailCartList";
+import CocktailCartList from "../../../../../components/cocktail/cocktailCartList";
+import OrderButton from "../../../../../components/shop/order_button";
 
 async function getCocktailList(cocktailIDs: number[]) {
+    if (cocktailIDs.length === 0) return[];
+    
     let param = '?';
     for (let i = 0; i < cocktailIDs.length; i++) {
         param += 'ids[' + i + ']=' + cocktailIDs[i] + '&';
@@ -12,7 +15,7 @@ async function getCocktailList(cocktailIDs: number[]) {
     return res.json();
 }
 
-export default function Page({params}: { params: { shopId: number } }) {
+export default function Page({params}: { params: { shopId: number, tableId: number } }) {
     const [cocktailList, setCocktailList] = useState([]);
     
     const cocktail = async () => {
@@ -32,13 +35,19 @@ export default function Page({params}: { params: { shopId: number } }) {
         cocktail();
     }, []);
     
+    const cocktailCartList = cocktailList.length === 0 ?
+        <></>
+        : <CocktailCartList cocktailList={cocktailList} category={'shop'} shopID={params.shopId}/>
+    
+    
     return (
         <RootLayout
             page={{name: 'shop', id: params.shopId}}
             children={
-                cocktailList.length === 0 ?
-                    <></>
-                    : <CocktailCartList cocktailList={cocktailList} category={'shop'} shopID={params.shopId}/>
+            <>
+                {cocktailCartList}
+                <OrderButton shopId={String(params.shopId)} tableId={String(params.tableId)} />
+            </>
             }
         />
     )
