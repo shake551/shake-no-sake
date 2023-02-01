941dbf30bf43fe1d32946c02262da14ae27bf3de
shake551 <73379887+shake551@users.noreply.github.com>
2023-02-01 23:46:28 +0900

    add order button

---
+++ src/app/components/shop/order_button.tsx
@@ -0,0 +1,34 @@
+import styles from './order_button.module.css';
+
+async function order(shopId: string, tableId: string) {
+    const exists = localStorage.getItem('cart');
+    let cocktailIDs = [];
+    if (exists) {
+        const data = JSON.parse(exists)
+        for (let i = 0; i < data.length; i++) {
+            cocktailIDs.push(parseInt(data[i]['id']));
+        }
+    }
+    console.log(JSON.stringify(cocktailIDs))
+    const res = await fetch(`http://localhost/shop/${shopId}/table/${tableId}/order`,
+        {
+            method: 'POST',
+            body: JSON.stringify({cocktail_ids: cocktailIDs}),
+            headers: {
+                'Content-Type': 'application/json'
+            },
+        });
+    
+    if (res.ok) {
+        localStorage.removeItem('cart');
+        window.location.replace(`/shop/${shopId}/cocktail`);
+    }
+}
+
+export default function OrderButton(params: { shopId: string, tableId: string }) {
+    return (
+        <>
+            <button onClick={async () => order(params.shopId, params.tableId)} className={styles.orderButton} >注文する</button>
+        </>
+    )
+}
